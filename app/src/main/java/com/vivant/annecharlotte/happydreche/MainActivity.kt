143e9165c416142b31ebc31b11e2ecf3e933de0f
ACVivant <annecharlotte.vivant@gmail.com>
2019-12-31 13:28:20 +0100

    add geolocalisation

--- app/src/main/java/com/vivant/annecharlotte/happydreche/MainActivity.kt
+++ app/src/main/java/com/vivant/annecharlotte/happydreche/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.vivant.annecharlotte.happydreche
 
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationProvider
+import android.os.Build
 import android.os.Bundle
+import android.os.Looper
 //import android.support.design.widget.Snackbar
 //import android.support.design.widget.NavigationView
 //import android.support.v4.view.GravityCompat
@@ -12,22 +17,43 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.widget.Toolbar;
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.GravityCompat
+import com.facebook.places.internal.LocationPackageRequestParams
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.navigation.NavigationView
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+    private var latitude:Double=0.toDouble()
+    private var longitude:Double=0.toDouble()
+    private lateinit var mLastLocation:Location
+    private var mMarker:Marker?=null
+
+    //Location
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    lateinit var locationRequest: LocationRequest
+    lateinit var  locationCallback: LocationCallback
+
+    companion object {
+        private const val MY_PERMISSION_CODE: Int = 1000
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +63,32 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        //Request runtime permission
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkLocationPermission()) {
+                buildLocationRequest();
+                buidLocationCallback();
+
+                fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+                fusedLocationProviderClient.requestLocationUpdates(
+                    locationRequest,
+                    locationCallback,
+                    Looper.myLooper()
+                );
+            }
+        }
+        else {
+            buildLocationRequest();
+            buidLocationCallback();
+
+            fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+            fusedLocationProviderClient.requestLocationUpdates(
+                locationRequest,
+                locationCallback,
+                Looper.myLooper()
+            )
+        }
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -50,6 +102,80 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         toggle.syncState()
 
         nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    private fun buidLocationCallback() {
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(p0: LocationResult?) {
+                mLastLocation = p0!!.locations.get(p0!!.locations.size-1) //Get last location
+
+                if (mMarker != null) {
+                    mMarker!!.remove()
+                }
+
+                latitude = mLastLocation.latitude
+                longitude = mLastLocation.longitude
+
+                val latLng = LatLng(latitude, longitude)
+                val markerOptions = MarkerOptions()
+                    .position(latLng)
+                    .title("your position")
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE))
+                mMarker = mMap!!.addMarker(markerOptions)
+
+                //Move camera
+                mMap!!.moveCamera(CameraUpdateFactory.newLatLng(latLng))
+                mMap!!.animateCamera(CameraUpdateFactory.zoomTo(11f))
+            }
+        }
+    }
+
+    private fun buildLocationRequest() {
+        locationRequest = LocationRequest()
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationRequest.interval = 5000
+        locationRequest.fastestInterval = 3000
+        locationRequest.smallestDisplacement = 10f
+    }
+
+    private fun checkLocationPermission(): Boolean {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.ACCESS_FINE_LOCATION))
+                ActivityCompat.requestPermissions(this, arrayOf(
+                    android.Manifest.permission.ACCESS_FINE_LOCATION
+                ), MY_PERMISSION_CODE)
+            else
+                ActivityCompat.requestPermissions(this, arrayOf(
+                    android.Manifest.permission.ACCESS_FINE_LOCATION
+                ), MY_PERMISSION_CODE)
+            return false
+        }
+        else return true
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            MY_PERMISSION_CODE -> {
+                if (grantResults.size>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+                        if (checkLocationPermission()) {
+                            buildLocationRequest();
+                            buidLocationCallback();
+
+                            fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+                            fusedLocationProviderClient.requestLocationUpdates(
+                                locationRequest,
+                                locationCallback,
+                                Looper.myLooper())
+
+                            mMap!!.isMyLocationEnabled = true
+                        }
+                }
+                else {
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
     }
 
     override fun onBackPressed() {
@@ -100,11 +226,32 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         return true
     }
 
+    override fun onStop() {
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+        super.onStop()
+    }
+
     override fun onMapReady(googleMap: GoogleMap) {
        mMap = googleMap
 
-        val Lille = LatLng(50.63, 3.07)
+        // Init Goole Play Services
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                mMap!!.isMyLocationEnabled = true
+            }
+        }
+        else
+            mMap!!.isMyLocationEnabled = true
+
+        // Enable zoom control
+        mMap.uiSettings.isZoomControlsEnabled = true
+
+        /*val Lille = LatLng(50.63, 3.07)
         mMap.addMarker(MarkerOptions().position(Lille).title("Happy Lille"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(Lille))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Lille))*/
     }
 }
